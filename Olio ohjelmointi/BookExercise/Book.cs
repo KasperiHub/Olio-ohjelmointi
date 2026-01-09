1ea9879f902992ba4c9d5af4192c0b3da945f42d
KasperiHub <kasperi.nyrkko@student.lab.fi>
2026-01-09 10:52:33 +0200

    update

--- Olio ohjelmointi/BookExercise/Book.cs
+++ Olio ohjelmointi/BookExercise/Book.cs
@@ -8,6 +8,44 @@ namespace Olio_ohjelmointi.BookExercise
 {
     internal class Book
     {
+        public string title;
+        public string author;
+        public double price;
+        public string id;
 
-    }
+        public Book()
+        {
+            this.title = string.Empty;
+            this.author = string.Empty;
+            this.price = 0;
+            this.id = string.Empty;
+
+        }
+
+        public Book(string title, string author, double price, string id)
+        {
+            this.title = title;
+            this.author = author;
+            this.price = price;
+            SetId(id); // this.id = id;
+        }
+
+        public void PrintBookInfo()
+        {
+            Console.WriteLine($"{this.author} : {this.title} : {this.price} : {this.id}");
+        }
+        public void SetId(string idvalue)      
+        {
+            if (idvalue.Length == 5)
+            {
+                this.id = idvalue;
+            }
+            else
+            {
+                this.id = " ";
+            }
+        }
+            
+    }        
 }
+
