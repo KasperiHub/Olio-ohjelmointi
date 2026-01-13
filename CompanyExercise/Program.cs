50e2673105f9a28a86afb8d31d4d392988cebc30
KasperiHub <kasperi.nyrkko@student.lab.fi>
2026-01-13 19:49:07 +0200

    moi

---
+++ CompanyExercise/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CompanyExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
